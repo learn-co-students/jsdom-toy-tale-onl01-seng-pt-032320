5c0001ba87bf55126a8c07e837572127be126614
HMoreira2020 <moreiraheather@gmail.com>
2020-09-22 21:21:03 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,101 @@
 let addToy = false;
+const toyCollection = document.getElementById('toy-collection');
+const createToyForm = document.querySelector(".add-toy-form");
+
+let toys; 
+const URL = "http://localhost:3000/toys";
+
+function fetchToys() {
+  return fetch("http://localhost:3000/toys")
+  .then(function(response) {
+  return response.json();
+  }).then(function(toys) {
+    renderToys(toys);
+  })
+};
+
+
+
+function renderToys(toys){
+  toys.forEach(toy => {
+    let divCard = document.createElement('div');
+    divCard.className = "class";
+    let nameHeader = document.createElement('h2');
+    let imageTag = document.createElement('img');
+    imageTag.className = "toy-Avatar";
+    let likesP = document.createElement('p');
+    let buttonTag = document.createElement('button');
+    buttonTag.className = "like-btn";
+    buttonTag.innerHTML = "Like <3";
+    buttonTag.addEventListener('click', (e) => {
+      e.preventDefault();
+      fetch(`${URL}/${toy.id}`, {
+        method: 'PATCH',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(
+          {"likes": toy.likes += 1}
+        )
+      })
+      .then(response => response.json())
+      .then(json => {
+        likesP.innerHTML = `likes: ${toy.likes}`;
+    });
+    nameHeader.innerHTML = toy.name;
+    imageTag.src = toy.image;
+    likesP.innerHTML = `likes: ${toy.likes}`;
+    let divs = [nameHeader, imageTag, likesP, buttonTag]; 
+    divs.forEach(div => divCard.appendChild(div));
+    toyCollection.appendChild(divCard);
+  });
+  renderToys(toys);
+});
+
+
+
+
+function submitData(name, image){
+  let formData = {
+    name: name,
+    image: image
+  };
+  
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+    };
+  
+  return fetch("http://localhost:3000/toys", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object)
+  })
+  .catch(function(error) {
+    alert("error!");
+    toyCollection.innerHTML = error.message;
+  })
+};
+
+
+createToyForm.addEventListener("submit", (e) => {
+  submitData(e.target.name.value, e.target.image.value)
+});
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  fetchToys();
+  
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
@@ -12,4 +103,5 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
 });
