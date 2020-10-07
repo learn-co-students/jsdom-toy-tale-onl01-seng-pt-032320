d6db0223d7bd2994a660684c2e8187705890aa94
Shamel Lakin <christlife4u@gmail.com>
2020-10-07 18:34:05 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  fetch("http://localhost:3000/toys")
+  .then(r => r.json())
+  .then(toys => {
+    // take my toys array make them HTML in order to add them to the DOM
+  })
+
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
+    // code here
     if (addToy) {
       toyFormContainer.style.display = "block";
     } else {
