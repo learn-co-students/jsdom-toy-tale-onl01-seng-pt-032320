32426c608f904de86a6411bcc939e1cda3806472
Shamel Lakin <christlife4u@gmail.com>
2020-10-07 19:50:23 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ document.addEventListener("DOMContentLoaded", () => {
   .then(r => r.json())
   .then(toys => {
     // take my toys array make them HTML in order to add them to the DOM
+    let toysHTML = toys.map(function(toy){
+      return ` 
+      <div class="card">
+        <h2>${toy.name}</h2>
+        <img src=${toy.image} class="toy-avatar" />
+        <p>${toy.likes} Likes </p>
+        <button class="like-btn">Like <3</button>
+      </div>
+      `
+
+    })
+   document.querySelector("#toy-collection").innerHTML = toysHTML.join('')
+    
   })
 
 
