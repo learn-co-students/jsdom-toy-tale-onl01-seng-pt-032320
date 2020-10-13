bfc53e7a887d1bb3fb89462de24517f02503b67d
pledgedavis <pledgedavis14@gmail.com>
2020-10-13 15:53:52 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 let addToy = false;
 
+
+ function getAndyToys(){
+  fetch('http://localhost:3000/toys')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data) 
+    const toycollec = document.getElementById("toy-collection")
+       data.map{toy => {toycollec.appendChild(`<div class="card"> ${toy.name} <div/>`)} }
+  
+  });
+ }
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
+
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
